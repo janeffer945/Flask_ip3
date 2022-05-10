81499462d0609f7b441d45ddd53c9e4cef07bfba
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 02:30:35 +0300

    add flask login

--- app/models.py
+++ app/models.py
@@ -1,2 +1,9 @@
-from app import create_app,db
-from app.models import User
+# from app import create_app,db
+# from app.models import User
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from datetime import datetime
+from flask_login import UserMixin,current_user
+
+
