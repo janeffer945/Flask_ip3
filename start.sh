4852fea16282bc72cc35c0e54735b7f869b10437
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 02:01:04 +0300

    import app

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=555666777
-export MAIL_USERNAME=janeffernjeri945@gmail.com
-export MAIL_PASSWORD=janeffernjerikinyua
+# export MAIL_USERNAME=janeffernjeri945@gmail.com
+# export MAIL_PASSWORD=janeffernjerikinyua
 export DATABASE_URL='postgresql+psycopg2://moringa:janeffer@localhost/pitches'
 python3.8 manage.py server
 
