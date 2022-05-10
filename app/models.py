10ee136796686a0ec0630e9550d0f8d3faf403d4
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 02:31:58 +0300

    class user db,model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,17 @@ from datetime import datetime
 from flask_login import UserMixin,current_user
 
 
+class User(UserMixin,db.Model):
+    __tablename__='users'
+
+    id= db.Column(db.Integer,primary_key=True)
+    username= db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+
