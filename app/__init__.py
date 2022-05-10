284e64d3e670d88138571efecbe25b103249d590
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 02:54:06 +0300

    creating app

--- app/__init__.py
+++ app/__init__.py
@@ -14,3 +14,15 @@ db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
 
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
