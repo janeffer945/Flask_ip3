dfc128a14f78a37faa49e3020c06b6069dd0f23c
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 02:53:18 +0300

    add string

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,12 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+
+bootstrap=Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
