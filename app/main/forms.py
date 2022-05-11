747a4648c6cd85a45642c009d8ffe0d62c0a01c1
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 03:06:48 +0300

    class pitchform

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import DataRequired
+# from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Interview','Interview'),('Product','Product'),('Promotion','Promotion ')],validators=[DataRequired()])
+    post = TextAreaField('Your Pitch', validators=[DataRequired()])
+    submit = SubmitField('Pitch')
