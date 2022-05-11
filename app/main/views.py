ab3f42fb826370ed8313b0ff7ed6da458e0dbaff
janeffer945 <janeffernjeri945@gmail.com>
2022-05-11 03:10:57 +0300

    define profile

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,21 @@ from ..models import Pitch, User, Comment, Upvote, Downvote
 
 from .forms import UpdateProfile,PitchForm,CommentForm,UpvoteForm
 # from .forms import UpdateProfile,PitchForm,CommentForm
+@main.route('/')
+def index():
+    pitches = Pitch.query.all()
+    interview = Pitch.query.filter_by(category = 'Interview').all() 
+    product = Pitch.query.filter_by(category = 'Product').all()
+    promotion = Pitch.query.filter_by(category = 'promotion').all()
+
+    return render_template('index.html', interview = interview,product = product, pitches = pitches,promotion= promotion)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
